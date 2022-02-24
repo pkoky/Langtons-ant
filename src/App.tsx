9f4b7a8a5f5b8a8a0ff1072d9ec9144a64f51b86
KoheiIshida <sscsk2224@gmail.com>
2022-02-24 21:11:55 +0900

    FrontPageをimport

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import FrontPage from './pages/Front';
 
 function App() {
   return (
     <div>
       <Button>Langton's ant</Button>
+      <FrontPage />
     </div>
   );
 }
--- src/pages/Front.tsx
+++ src/pages/Front.tsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+
+const FrontPage = () => {
+  return (
+    <div>
+      <h1>Front Page</h1>
+    </div>
+  )
+}
+
+export default FrontPage;
